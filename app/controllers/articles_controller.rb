20b6ace7885963861e68160b49d4a1b827cba0ee
Samuel Banya <sbanya@fastmail.com>
2022-11-03 19:45:42 -0500

    commit tree 30787d8f34e9947a72b3a1d950c92282b6962cd3

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    session[:page_views] || = 0
+    session[:page_views] ||= 0
     session[:page_views] += 1
 
     if session[:page_views] <= 3
